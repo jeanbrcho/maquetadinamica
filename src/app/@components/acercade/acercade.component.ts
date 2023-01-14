b269cfc84cfe23b98dfaefb349dedd564a1b0145
jeanbrcho <jeancbs18@gmail.com>
2023-01-14 18:18:50 -0300

    servicios

--- src/app/@components/acercade/acercade.component.ts
+++ src/app/@components/acercade/acercade.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/servicios/datos.service';
 
 @Component({
   selector: 'app-acercade',
   templateUrl: './acercade.component.html',
   styleUrls: ['./acercade.component.css']
 })
-export class AcercadeComponent {
+export class AcercadeComponent implements OnInit {
+  acercade : any = [];
+  constructor(private datos:DatosService) {}
 
+  ngOnInit(): void {
+    this.datos.getDatos().subscribe(data => {
+      this.acercade = data.acercade;
+  })
+
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AcercadeModule } from './@components/acercade/acercade.module';
 import { ContactoModule } from './@components/contacto/contacto.module';
 import { EditaracercadeModule } from './@components/editaracercade/editaracercade.module';
@@ -27,6 +29,7 @@ import { AppComponent } from './app.component';
     HabilidadesModule,
     NavbarModule,
     ProyectosModule,
+    HttpClientModule,
 
   ],
   providers: [],
